8c36efa2a6e31cceebfeae5ee9b5dd855531025e
Jaemin Kim <jaeminkim90@gmail.com>
2022-01-08 18:06:47 +0900

    TEST: 스프링부트 기초 MVC 실습 - findAll() 테스트 코드 구현

--- src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -1,15 +1,24 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import javax.swing.*;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 
 class MemoryMemberRepositoryTest {
 
-    MemberRepository repository = new MemoryMemberRepository();
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach() {
+
+        repository.clearStore(); // 모든 테스트의 실행마다 반복 실행된다.
+    }
 
     @Test
     public void save() {
@@ -36,5 +45,21 @@ class MemoryMemberRepositoryTest {
         Member result = repository.findByName("회원1").get();
 
         assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findAll() {
+        Member member1 = new Member();
+        member1.setName("회원1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("회원2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+        assertThat(result.size()).isEqualTo(2);
+
+
     }
 }
