d34c3e7c1a63f8c7e75845498fd1b7d4f231af27
Jaemin Kim <jaeminkim90@gmail.com>
2022-01-08 17:36:44 +0900

    스프링부트 기초 MVC 실습 - TEST findByname 테스트  코드 구현

--- src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -3,6 +3,8 @@ package hello.hellospring.repository;
 import hello.hellospring.domain.Member;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.*;
 
 class MemoryMemberRepositoryTest {
@@ -19,5 +21,20 @@ class MemoryMemberRepositoryTest {
         Member result = repository.findById(member.getId()).get();
         assertThat(member).isEqualTo(result);
 
+    }
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("회원1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("회원2");
+        repository.save(member2);
+
+        Member result = repository.findByName("회원1").get();
+
+        assertThat(result).isEqualTo(member1);
     }
 }
