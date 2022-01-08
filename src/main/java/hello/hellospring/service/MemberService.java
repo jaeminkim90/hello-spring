67147d6b93ebb264446fed8855fc4ad434a51f1d
Jaemin Kim <jaeminkim90@gmail.com>
2022-01-08 19:37:27 +0900

    FEAT: 스프링부트 기초 MVC 실습 - 회원 가입, 중복회원 검증 기능 추가

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,36 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member) {
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    // 같은 이름이 있는 중복 회원 가입 X
+    // findByName()의 반환 객체가 Optional로 감싸있기 때문에
+    // ifPresent를 이용하여 값이 존재하는지 확인할 수 있다.
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다");
+                });
+    }
+
+    p
+
+
+
+}
