4982aea5203f28d9d4ffe97b2eb9a27b9fd2bea2
Jaemin Kim <jaeminkim90@gmail.com>
2022-01-10 20:25:52 +0900

    TEST: 스프링과 테스트를 함께 실행하는 통합 테스트 구현

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -15,14 +15,16 @@ public class SpringConfig {
     public SpringConfig(DataSource dataSource) {
         this.dataSource = dataSource;
     }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
+
     @Bean
     public MemberRepository memberRepository() {
        //return new MemoryMemberRepository();
         return new JdbcMemberRepository(dataSource);
     }
-    //
+
 }
